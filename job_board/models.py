8459d59c19e3cd32e2b6cc56103b2f9106791e8b
Thabo <thvbojafta@gmail.com>
2024-08-21 22:55:32 +0200

    Add initial migration for JobPosting model

--- job_board/models.py
+++ job_board/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class JobPosting(models.Model):
+    title=models.CharField(max_length=50)
+    description = models.TextField()
+    company = models.CharField(max_length=50)
+    salary = models.IntegerField()
+    
